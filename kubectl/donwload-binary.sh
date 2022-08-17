06f53f95743c3b0e841d85c7336414f1ecd72cd8
Silvio Silva <smsilva@gmail.com>
2022-08-17 15:57:11 -0300

    bump: version 0.0.1 → 0.1.0

--- kubectl/donwload-binary.sh
+++ kubectl/donwload-binary.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export KUBECTL_VERSION="${1-1.22.12}"
-export DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY="docker_build_temp"
+export DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY="./kubectl/docker_build_temp"
 export KUBECTL_DOWNLOADED_BINARY="${DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY?}/kubectl_downloaded_binary"
 
 mkdir -p "${DOCKERBUILD_CONTEXT_TEMPORARY_DIRECTORY?}"
@@ -10,10 +10,3 @@ if ! [ -e "${KUBECTL_DOWNLOADED_BINARY?}" ]; then
     --output-document ${KUBECTL_DOWNLOADED_BINARY?} \
     https://dl.k8s.io/release/v${KUBECTL_VERSION?}/bin/linux/amd64/kubectl
 fi
-
-echo "kubectl:"
-echo "  version: ${KUBECTL_VERSION}"
-
-docker build \
-  --tag kubectl:latest \
-  .
