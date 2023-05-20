59e103befd6feb14c99c745a887808d2d318c2a3
Silvio Silva <smsilva@gmail.com>
2023-05-20 12:19:54 -0300

    feat(kafka/): Add Kafka Docker Get Started

---
+++ kafka/commands.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+docker compose up -d
+
+docker exec broker \
+  kafka-topics \
+    --bootstrap-server broker:9092 \
+    --create \
+    --topic quickstart
+
+docker exec \
+  --interactive \
+  --tty broker \
+  kafka-console-producer \
+    --bootstrap-server broker:9092 \
+    --topic quickstart
+
+docker exec \
+  --interactive \
+  --tty broker \
+  kafka-console-consumer \
+    --bootstrap-server broker:9092 \
+    --topic quickstart \
+    --from-beginning
+
+docker compose down
